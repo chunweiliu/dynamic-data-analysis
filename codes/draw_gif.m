711f3076fa44a2634df9a7e0cba226c80ba3de8e
Chun-Wei Liu <chunwei@cs.unc.edu>
2014-10-27 22:38:56 -0400

    Visualize one slide of dynamic data

---
+++ codes/draw_gif.m
@@ -0,0 +1,15 @@
+function draw_gif(filename, i)
+drawnow
+
+set(gcf, 'color', [1 1 1])
+
+frame = getframe(1);
+im = frame2im(frame);
+[imind, cm] = rgb2ind(im,256);
+
+if i == 1
+    imwrite(imind,cm,filename,'gif','Loopcount',inf);
+else
+    imwrite(imind,cm,filename,'gif','WriteMode','append');
+end
+end
---
+++ codes/vis_nrrd2gif.m
@@ -0,0 +1,26 @@
+function vis_nrrd2gif(input_folder, output_path)
+% VIS_NRRD2GIF generate gif annimation from 2D slides in nrrd
+% inputs
+% - input_folder, folder which stored nrrd files
+% - output_file, output gif file
+
+close all
+
+files = dir(fullfile(input_folder,'*.nrrd'));
+for i = 1:length(files)    
+    
+    subject_file = fullfile(input_folder, files(i).name);
+    fprintf('process %s\n', subject_file)
+    
+    [im, ~] = nrrdread(subject_file);
+    
+    % rectify image
+    slice = squeeze(im(:,size(im,2)/2,:));  % remove singluar axis
+    slice = mat2gray(imresize(imrotate(slice,90),[512 512]));  % rescale
+    slice = slice.^.7;  % addjust brightnest
+    
+    % draw as gif
+    imshow(slice)
+    draw_gif(output_path, i)
+    
+end
